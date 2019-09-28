a511136432d09f5f4110198ffdd7eba3018baebb
ConnorBrereton <breretonconnor@gmail.com>
2019-09-27 18:32:35 -0700

    Updates

---
+++ precache-manifest.d9454c8af7f91cf6a207c1885fe91d5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08e096152411e7f3360d77f854442ec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "480abab775ea0a3582da",
+    "url": "/monsters-rolodex/static/css/main.89626d33.chunk.css"
+  },
+  {
+    "revision": "6b610ca0911a0dcf307b",
+    "url": "/monsters-rolodex/static/js/2.84fd0de3.chunk.js"
+  },
+  {
+    "revision": "480abab775ea0a3582da",
+    "url": "/monsters-rolodex/static/js/main.c31cc166.chunk.js"
+  },
+  {
+    "revision": "0d0f416e7b5fe29edb74",
+    "url": "/monsters-rolodex/static/js/runtime-main.9ce5b26a.js"
+  }
+]);
